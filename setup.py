d6b0d6602f446df755e5eab200264d89a1738f9c
astujindal <astutijindal2016@gmail.com>
2022-01-27 14:54:19 +0530

    webapp added and updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name = "src",
+    version ="0.0.1",
+    description ="Its Wine Q package",
+    author ="Astuti Jindal",
+    packages = find_packages(),
+    license="Oracle"
+
+)
