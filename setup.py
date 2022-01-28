5b514945cb647f9b705ad38b8fbefd59b59518ff
astujindal <astutijindal2016@gmail.com>
2022-01-28 12:01:47 +0530

    CML

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 setup(
     name = "src",
-    version ="0.0.1",
-    description ="Its Wine Q package",
-    author ="Astuti Jindal",
+    version = "0.0.1",
+    description = "Its Wine Q package ",
+    author = "Dr.Rao",
     packages = find_packages(),
-    license="Oracle"
-
+    license="MIT"
 )
